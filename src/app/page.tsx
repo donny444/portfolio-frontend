df9adb288c21d4b6e15475c0f615761bbe716f64
Donny <tanadornrach@gmail.com>
2025-07-16 22:48:09 +0700

    first iteration of root page and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,110 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
+import { Button } from "react-bootstrap";
 
-export default function Home() {
+import biographyData from "@/data/biography.json";
+import projectsData from "@/data/projects.json";
+import educationData from "@/data/education.json";
+
+import SkillsSection from "@/app/skills/skills_section.tsx";
+import ContactsSection from "@/app/contacts/contacts_section.tsx";
+
+export default function BiographyPage(): React.ReactNode {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <p>Hello Next.js</p>
+      <Profile fileName="/profile.jpg" />
+      <BiographyBox />
+      <EducationBox />
+      <ProjectsBox />
+      <SkillsSection />
+      <ContactsSection />
+    </div>
+  );
+}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+function Profile({ fileName }: { fileName: string }): React.ReactNode {
+  return (
+    <div className="profile">
+      <Image src={fileName} alt="sample profile image" width={50} height={50} />
+      <h3 className="profile-text">Welcome to my portfolio website!</h3>
+    </div>
+  );
+}
+
+function BiographyBox(): React.ReactNode {
+  const firstName = biographyData[0].firstname;
+  const lastName = biographyData[0].lastname;
+  const nickName = biographyData[0].nickname;
+  const birthDate = new Date(biographyData[0].birthdate.$date);
+  const nationality = biographyData[0].nationality;
+
+  const currentDate = new Date();
+  const diffInMilliseconds = currentDate.getTime() - birthDate.getTime();
+  const age = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
+
+  return (
+    <div className="box">
+      <h3 className="box-header">Biography</h3>
+      <p className="box-label">Name:</p>
+      <p className="box-value">
+        {firstName} {lastName} ({nickName})
+      </p>
+      <p className="box-label">Birthdate:</p>
+      <p className="box-value">
+        0{birthDate.getDate()}/0{birthDate.getMonth() + 1}/
+        {birthDate.getFullYear()} ({age})
+      </p>
+      <p className="box-label">Nationality:</p>
+      <p className="box-value">{nationality}</p>
+    </div>
+  );
+}
+
+function ProjectsBox(): React.ReactNode {
+  return (
+    <div>
+      <h3>All my side projects</h3>
+      <div>
+        {projectsData.map((project, index) => (
+          <div key={index}>
             <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={`/${project.file_name}`}
+              alt={project.name}
+              width={50}
+              height={50}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+            <p>{project.name}</p>
+          </div>
+        ))}
+      </div>
+      <Button variant="primary">
+        <Link href="/projects">More Information</Link>
+      </Button>
+    </div>
+  );
+}
+
+function EducationBox(): React.ReactNode {
+  return (
+    <div className="box">
+      <h3 className="box-header">See my education profile</h3>
+      <div className="education-images">
+        {educationData.map((institute, index) => (
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="education-image"
+            key={index}
+            src={`/${institute.file_name}`}
+            alt={institute.name}
+            width={50}
+            height={50}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))}
+      </div>
+      <Button variant="primary">
+        <Link className="box-link" href="/education">Click</Link>
+      </Button>
     </div>
   );
 }
